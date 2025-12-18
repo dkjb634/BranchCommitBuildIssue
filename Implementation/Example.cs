1d69873726b4ed1723fd2ced2f0471c9a1ce0811
Dmitry.Kazantsev <dmitry.kazantsev@jetbrains.com>
2025-12-18 17:56:38 +0100

    qwe

--- Implementation/Example.cs
+++ Implementation/Example.cs
@@ -6,6 +6,6 @@ public class Example: IExample {
 	public ExampleReturnRecord GetExampleReturnRecord()
 	{
 		Console.WriteLine("QWE");
-		return new ExampleReturnRecord("QWE");
+		return new ExampleReturnRecord("QWE", "qwe");
 	} 
 }
